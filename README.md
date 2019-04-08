cc082e8f8d5b2fee45307494aa473dfd87004c7c
Eduardo Gonzalez <jose_e_gonzalez@homedepot.com>
2019-04-08 13:10:33 -0400

    Update dependencies and improve readme file

--- README.md
+++ README.md
@@ -9,12 +9,35 @@ Demo & Resources
 Development server
 ===================
 
-After cloning this repository go to the root of the project folder and run following commands.
-```
-npm start
-npm start:server
-```
+## Installation
+1. Clone this repo: `git clone https://github.com/it6203group1/social-media-app.git`
+2. `cd` into the folder of the repo you just cloned
+3. Install npm dependencies `npm install`
+4. Start a MongoDB instance `brew services start mongodb`
+5. You're ready to go; `npm start` to run the application.
 
 Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 Windows users please use [Git Bash](https://stackoverflow.com/a/37478310) or [Cmder](http://cmder.net/) to run the commands.
+
+## Deployment
++ `npm start` - Launch development server of Angular Cli and server.js node app through nodemon for live restart
++ `npm run build` - Build the ng project into the dist folder. In production you will just start the server.js file
+
+Stack Technologies
+===================
+### Back-end
++ Node.js
++ Express 4
++ Mongoose to handle MongoDB connections
+
+### Front-end
++ Angular + Angular Material
++ Angular Cli
++ TypeScript
+
+#### Development modules
++ Nodemon for live server restart
++ Concurrently for running and handling multiple npm packages
++ Morgan as logging library
++ Angular-cli to handle all the angular stuff
--- server.js
+++ server.js
@@ -1,15 +1,49 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var compression = require("compression");
-var cors = require("cors");
-var app = express();
+const express = require('express');
+const passport = require('passport');
+const helmet = require('helmet');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const chalk = require('chalk');
+const errorHandler = require('errorhandler');
+const dotenv = require('dotenv');
+const path = require('path');
+const mongoose = require('mongoose');
+const expressValidator = require('express-validator');
 
-app.set("port", process.env.PORT || 3000);
+const app = express();
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured only if NODE_ENV var is set to production.
+ */
+dotenv.config({silent: process.env.NODE_ENV === 'production'});
+dotenv.load({path: '.env'});
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI, { useNewUrlParser: true });
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT || 3000);
+app.use(helmet())
+app.use(compression());
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({ credentials: true, origin: true }));
-app.use(compression());
+app.use(expressValidator());
 
+/**
+ * API examples routes.
+ */
 app.get("/api", function(req, res) {
   // res.json(version);
   res.send('Hello World!');
@@ -19,12 +53,37 @@ app.all("*", function(req, res) {
   res.redirect("/");
 });
 
-app.listen(app.get("port"), function() {
-  console.log(
-    "App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
+/**
+ * Error Handler.
+ */
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorhandler())
+}
+
+/**
+ * Catch all the routes and give back the Angular app
+ */
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./public/dist/'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/dist/index.html'));
+  });
+}
+
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({
+      "message": err.name + ": " + err.message
+    });
+  }
+});
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+  console.log('  Press CTRL-C to stop\n');
 });
 
 module.exports = app;
