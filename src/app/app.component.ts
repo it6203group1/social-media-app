53a4092a096df6c2b403a0d16a9490f22dca78b4
mray44 <48001356+mray44@users.noreply.github.com>
2019-04-22 20:22:43 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'social-media-app';
+  title = 'Social Media Management App';
 }
